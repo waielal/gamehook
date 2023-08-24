a88dfa5dc4d0d52836b7111e7dbe364aa85e38c9
Flame Sage <chris062689@gmail.com>
2023-08-24 17:45:38 -0400

    Added initial shell of MemoryContainer.

---
+++ src/GameHook.Domain/Implementations/MemoryContainer.cs
@@ -0,0 +1,60 @@
+﻿using GameHook.Domain.Interfaces;
+
+namespace GameHook.Domain.Implementations
+{
+    public class MemoryContainer : IMemoryContainer
+    {
+        private Dictionary<MemoryAddress, byte[]> MemoryLocations { get; }
+
+        public MemoryContainer()
+        {
+            MemoryLocations = new Dictionary<MemoryAddress, byte[]>();
+        }
+
+        public MemoryAddress GetRelativeAddress(MemoryAddress absoluteAddress)
+        {
+            return 0x00;
+        }
+
+        public MemoryAddress GetAbsoluteAddress(MemoryAddress relativeAddress)
+        {
+            return 0x00;
+        }
+
+        public byte[] GetBytes(MemoryAddress address, int length)
+        {
+            var baseAddress = (uint)0x00;
+            return MemoryLocations[baseAddress][(int)(baseAddress - address)..length].ToArray();
+        }
+
+        public ulong GET_DATA8_LE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GET_DATA8_BE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GET_DATA16_LE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GET_DATA16_BE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GET_DATA32_LE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong GET_DATA32_BE(MemoryAddress address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/GameHook.Domain/Interfaces/IMemoryContainer.cs
@@ -0,0 +1,16 @@
+﻿namespace GameHook.Domain.Interfaces
+{
+    public interface IMemoryContainer
+    {
+        public byte[] GetBytes(MemoryAddress address, int length);
+
+        public ulong GET_DATA8_LE(MemoryAddress address);
+        public ulong GET_DATA8_BE(MemoryAddress address);
+
+        public ulong GET_DATA16_LE(MemoryAddress address);
+        public ulong GET_DATA16_BE(MemoryAddress address);
+
+        public ulong GET_DATA32_LE(MemoryAddress address);
+        public ulong GET_DATA32_BE(MemoryAddress address);
+    }
+}
